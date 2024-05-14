855ded6e3bb16ee561208e82a6cd1fc2bb1a73ed
Mariano-Pozo <mlxito.nax2@gmail.com>
2024-05-14 19:17:10 -0300

    primer comit

---
+++ Entidades/Entidades/Persona.cs
@@ -0,0 +1,52 @@
+﻿namespace Entidades
+{
+    public abstract class Persona
+    {
+        protected string apellido;
+        protected string barrioRecidencia;
+        protected DateTime nacimiento;
+        protected string nombre;
+
+        public Persona (string nombre, string apellido, DateTime nacimiento)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.nacimiento = nacimiento;
+        }
+        public Persona(string nombre, string apellido, DateTime nacimiento, string barrioRecidencia)
+            :this (nombre,apellido,nacimiento)
+        {
+            this.barrioRecidencia = barrioRecidencia;
+        }
+
+
+        public int Edad 
+        {
+            get
+            {
+                return DateTime.Today.AddTicks(-this.nacimiento.Ticks).Year - 1
+            }
+        }
+        public string NombreCompleto 
+        {
+            get
+            {
+                return $"{apellido}, {nombre}";
+            }
+        }
+
+        internal abstract string FichaExtra()
+        {
+        }
+
+        public string FichaPersonal (Persona p)
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return NombreCompleto;
+        }
+    }
+}
---
+++ Entidades/form/Form1.cs
@@ -0,0 +1,12 @@
+using Entidades;
+
+namespace form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
